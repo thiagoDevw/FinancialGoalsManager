710c823aec1b3136cdf0b064ddd79d53affa21c1
Thiago Alexandre <thiagoasilvapro@gmail.com>
2024-10-17 22:58:45 -0300

    Novo commit

--- FinancialGoalsManager/Entities/FinancialObjective.cs
+++ FinancialGoalsManager/Entities/FinancialObjective.cs
@@ -5,12 +5,25 @@
         public int Id { get; set; }
         public string Titulo { get; set; }
         public decimal QuantidadeAlvo { get; set; }
-        public DateTime Prazo { get; set; } // Opcional
-        public decimal QuantIdealAporte { get; set; } // Opcional
-        public Enum Enum { get; set; } // Em progresso, Completo, Cancelado, Pausado
-        // Transações (Coleção de Transação)
+        public DateTime? Prazo { get; set; } // Opcional
+        public decimal? QuantIdealAporte { get; set; } // Opcional
+        public ObjectiveStatus Status { get; set; } 
         public DateTime DataCriacao { get; set; }
         public bool Deletado { get; set; }
 
+        // Valor total acumulado em transações
+        public decimal ValorTotal { get; set; }
+
+        // Coleção de transações
+        public ICollection<Transaction> Transacoes { get; set; }
+
+    }
+
+    public enum ObjectiveStatus
+    {
+        EmProgresso,
+        Completo,
+        Cancelado,
+        Pausado
     }
 }
