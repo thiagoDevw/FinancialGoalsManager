a17f71bbcc6cbb209eeb0eb73ae7c9c4864b077d
Thiago Alexandre <thiagoasilvapro@gmail.com>
2024-10-17 23:13:28 -0300

    Novo commit

--- FinancialGoalsManager/Entities/Transaction.cs
+++ FinancialGoalsManager/Entities/Transaction.cs
@@ -4,10 +4,19 @@
     {
         public int Id { get; set; }
         public decimal Quantidade { get; set; }
-        // public Enum (Deposito, Saque)
+        public TransactionType Tipo { get; set; }
         public DateTime DataTransaçao { get; set; }
         public DateTime DataCriaçao { get; set; }
         public bool Deletado { get; set; }
 
+        public int FinancialObjectiveIde {  get; set; }
+        public FinancialObjective FinancialObjective { get; set; }
+
+    }
+
+    public enum TransactionType
+    {
+        Deposito,
+        Saque
     }
 }
