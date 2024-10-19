dff489984f76274a5f26de93972e4f96bff89546
Thiago Alexandre <thiagoasilvapro@gmail.com>
2024-10-18 23:44:53 -0300

    Novo commit

--- FinancialGoalsManager/FinancialGoalsDbContext.cs
+++ FinancialGoalsManager/FinancialGoalsDbContext.cs
@@ -1,8 +1,14 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using FinancialGoalsManager.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace FinancialGoalsManager
 {
     public class FinancialGoalsDbContext : DbContext 
     {
+        public FinancialGoalsDbContext(DbContextOptions<FinancialGoalsDbContext> options)
+            : base(options) { }
+
+        public DbSet<FinancialObjective> Objectives { get; set; }
+        public DbSet<Transaction> Transactions { get; set; }
     }
 }
