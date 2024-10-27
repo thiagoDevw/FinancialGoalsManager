be3578c39030a5ea76a6e425f383892c30a60a59
Thiago Alexandre <thiagoasilvapro@gmail.com>
2024-10-27 19:33:07 -0300

    Novo commit

--- FinancialGoalsManager/Service/FinancialObjectiveService.cs
+++ FinancialGoalsManager/Service/FinancialObjectiveService.cs
@@ -6,5 +6,10 @@ namespace FinancialGoalsManager.Service
     public class FinancialObjectiveService
     {
         private readonly IRepository<FinancialObjective> _repository;
+
+        public FinancialObjectiveService(IRepository<FinancialObjective> repository)
+        {
+            _repository = repository;
+        }
     }
 }
