8c5303f05878e8fd70b08c5ba699c8b4b1895b83
Thiago Alexandre <thiagoasilvapro@gmail.com>
2024-10-30 21:50:22 -0300

    Novo commit

--- FinancialGoalsManager/Controllers/FinancialObjectiveController.cs
+++ FinancialGoalsManager/Controllers/FinancialObjectiveController.cs
@@ -1,9 +1,15 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FinancialGoalsManager.Service;
+using Microsoft.AspNetCore.Mvc;
 
 namespace FinancialGoalsManager.Controllers
 {
     public class FinancialObjectiveController : ControllerBase
     {
+        private readonly FinancialObjectiveService _service;
 
+        public FinancialObjectiveController(FinancialObjectiveService service)
+        {
+            _service = service;
+        }
     }
 }
