1d42dde66efa9118e90ae6af4f1d051c39b30790
Thiago Alexandre <thiagoasilvapro@gmail.com>
2024-11-21 19:13:34 -0300

    Novo commit

--- FinancialGoalsManager.Application/Models/FinancialObjective/FinancialObjectiveViewModel.cs
+++ FinancialGoalsManager.Application/Models/FinancialObjective/FinancialObjectiveViewModel.cs
@@ -2,8 +2,18 @@
 {
     public class FinancialObjectiveViewModel
     {
+        public FinancialObjectiveViewModel(int id, string titulo, decimal valorTotal)
+        {
+            Id = id;
+            Titulo = titulo;
+            ValorTotal = valorTotal;
+        }
+
         public int Id { get; set; }
         public string Titulo { get; set; }
         public decimal ValorTotal { get; set; }
+
+        public static FinancialObjectiveViewModel FromEntity(FinancialObjectiveViewModel entity)
+            => new FinancialObjectiveViewModel(entity.Id, entity.Titulo, entity.ValorTotal);
     }
 }
