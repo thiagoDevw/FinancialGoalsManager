29d3a4fcc841d27ba3704f33eda0d467de3693d9
Thiago Alexandre <thiagoasilvapro@gmail.com>
2024-11-28 22:08:39 -0300

    Novo commit

--- FinancialGoalsManager.Application/Models/Transaction/TransactionItemViewModel.cs
+++ FinancialGoalsManager.Application/Models/Transaction/TransactionItemViewModel.cs
@@ -18,5 +18,8 @@ namespace FinancialGoalsManager.Application.Models.Transaction
         public TransactionType Tipo { get; set; }
         public DateTime DataTransacao { get; set; }
         public DateTime DataCriacao { get; set; }
+
+        public static TransactionViewModel FromEntity(FinancialGoalsManager.Entities.Transaction entity)
+            => new TransactionViewModel(entity.Id, entity.Quantidade, entity.Tipo, entity.DataTransacao, entity.DataCriaçao);
     }
 }
