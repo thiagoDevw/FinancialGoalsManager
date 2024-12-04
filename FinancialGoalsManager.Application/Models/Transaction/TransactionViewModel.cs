d71e5e28a13575aa8dda6e051e420b38a272d049
Thiago Alexandre <thiagoasilvapro@gmail.com>
2024-12-03 21:05:20 -0300

    Novo commit

--- FinancialGoalsManager.Application/Models/Transaction/TransactionViewModel.cs
+++ FinancialGoalsManager.Application/Models/Transaction/TransactionViewModel.cs
@@ -4,9 +4,18 @@ namespace FinancialGoalsManager.Application.Models.Transaction
 {
     public class TransactionViewModel
     {
+        public TransactionViewModel(int id, decimal quantidade, TransactionType tipo)
+        {
+            Id = id;
+            Quantidade = quantidade;
+            Tipo = tipo;
+        }
+
         public int Id { get; set; }
         public decimal Quantidade { get; set; }
         public TransactionType Tipo { get; set; }
+
+
 
     }
 }
